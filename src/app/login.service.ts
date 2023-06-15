f1b207900a4fc9d3c0d72bdaa2ce76b1e3109f73
Joyel42 <ttc0820036@nttf.co.in>
2023-06-15 12:10:07 +0530

    ..

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -2,23 +2,17 @@ import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, Subject } from "rxjs";
 import jwt_decode from "jwt-decode";
-import { Router } from '@angular/router';
+
 @Injectable({
   providedIn: 'root'
 })
-export class LoginService implements OnInit{
+export class LoginService {
 
-  constructor(private httpclient:HttpClient, private router:Router){}
+  constructor(private httpclient:HttpClient){}
   
   api = 'http://testvedika.atai.ai/api/login/';
   isLogined:boolean=false;
 
-  ngOnInit(): void {
-    var a= localStorage.getItem("loginflag");  
-    if(a == "true"){ 
-      this.isLogined = true;
-    }
-  }
 
   apiresponse(logindetails):Observable<any>{
     return this.httpclient.post(this.api,logindetails);
